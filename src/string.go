e8741ade73d8f1e2c7ada13acd473beb1f9b61a8
Audie Milson <milson.audie@gmail.com>
2022-08-21 23:02:08 +0700

    Go-lang String Data Type

---
+++ src/string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Audie")
+	fmt.Println("Audie Milson")
+	// Function len
+	fmt.Println(len("Audie"))
+	// Function get character
+	fmt.Println("Audie Milson"[3])
+}
