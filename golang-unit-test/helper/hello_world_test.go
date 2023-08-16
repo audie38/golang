ad94e6d7143306d43c8abcd7a8c6a300b9378aa3
Audie Milson <milson.audie@gmail.com>
2023-08-16 23:02:56 +0700

    Add Command Line Notes  to Run test

--- golang-unit-test/helper/hello_world_test.go
+++ golang-unit-test/helper/hello_world_test.go
@@ -2,9 +2,22 @@ package helper
 
 import "testing"
 
+/*	Go Unit Test Command Line
+ *	go test = run all test
+ *	go test -v = to show what test functions running
+ *	go test -v ./... = to run all test from root folder
+ *	go test -v -run=[test functions name] = to run specific test functions
+ */
+
 func TestHelloWorld(t *testing.T){
 	result := HelloWorld("Audie")
 	if result != "Hello Audie"{
 		panic("Result is not Hello Audie")
+	}
+}
+func TestHelloWorldDummy(t *testing.T){
+	result := HelloWorld("Dummy")
+	if result != "Hello Dummy"{
+		panic("Result is not Hello Dummy")
 	}
 }
