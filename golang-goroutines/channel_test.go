a8cac57b256678e78f3cac4ca88dd44eb0462f59
Audie Milson <milson.audie@gmail.com>
2023-08-18 00:55:33 +0700

    Select channel

--- golang-goroutines/channel_test.go
+++ golang-goroutines/channel_test.go
@@ -96,3 +96,27 @@ func TestRangeChannel(t *testing.T){
 
 	fmt.Println("Done")
 }
+
+func TestSelectChannel(t *testing.T){
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+
+	for counter < 2{
+		select{
+		case data := <- channel1:
+			fmt.Println("Data dari channel 1: ", data)
+			counter ++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2: ", data)
+			counter ++
+		}
+	}
+}
