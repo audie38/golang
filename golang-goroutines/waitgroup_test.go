5fc53d025df2374d315fb6e981cb137d945261b8
Audie Milson <milson.audie@gmail.com>
2023-08-18 20:02:36 +0700

    better way to wait for all goroutine to finish

---
+++ golang-goroutines/waitgroup_test.go
@@ -0,0 +1,25 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func RunAsynchronous(group *sync.WaitGroup){
+	defer group.Done()
+
+	group.Add(1)
+	fmt.Println("Hello World")
+}
+
+func TestWaitGroup(t *testing.T){
+	group := &sync.WaitGroup{}
+
+	for i:= 0; i < 100; i++{
+		go RunAsynchronous(group)
+	}
+
+	group.Wait()
+	fmt.Println("Done...")
+}
