9210830ff48c092e46667edb22c2e92c8ad24f2e
Audie Milson <milson.audie@gmail.com>
2023-08-19 21:40:48 +0700

    Golang Context with deadline example

--- golang-context/context_test.go
+++ golang-context/context_test.go
@@ -93,6 +93,22 @@ func TestContextWithTimeout(t *testing.T){
 		}
 	}
 
+	fmt.Println("Total Goroutine: ", runtime.NumGoroutine())
+}
+
+func TestContextWithDeadline(t *testing.T){
+	fmt.Println("Total Goroutine: ", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithDeadline(parent, time.Now().Add(5 * time.Second))
+	defer cancel()
 	
+	destination := CreateCounter(ctx)
+	for n := range destination{
+		fmt.Println("Counter: ", n)
+		if n == 10{
+			break
+		}
+	}
+
 	fmt.Println("Total Goroutine: ", runtime.NumGoroutine())
 }
