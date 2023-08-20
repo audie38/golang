b5e7188755bc763c2eaf113aca1f42e07f0e9722
Audie Milson <milson.audie@gmail.com>
2023-08-20 21:48:28 +0700

    Golang Repository Pattern example

---
+++ golang-db/repository/comment_repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"context"
+	"golang_db/entity"
+)
+
+type CommentRepository interface {
+	Insert(ctx context.Context, comment entity.Comment) (entity.Comment, error)
+	FindAll(ctx context.Context) ([]entity.Comment, error)
+	FindById(ctx context.Context, id int64) (entity.Comment, error)
+	Update(ctx context.Context, comment entity.Comment) (entity.Comment, error)
+	Delete(ctx context.Context, id int64) (entity.Comment, error)
+}
