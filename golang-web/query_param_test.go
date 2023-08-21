4d693cc6c92d70bf8bb90a90e6e0d160aa24a51c
Audie Milson <milson.audie@gmail.com>
2023-08-21 22:49:48 +0700

    Get URL Multiple Query Parameter example

--- golang-web/query_param_test.go
+++ golang-web/query_param_test.go
@@ -17,12 +17,31 @@ func SayHello(writer http.ResponseWriter, request *http.Request){
 	}
 }
 
+func MultipleQueryParameter(writer http.ResponseWriter, request *http.Request){
+	firstName := request.URL.Query().Get("first_name")
+	lastName := request.URL.Query().Get("last_name")
+
+	fmt.Fprintf(writer, "Hello %s %s", firstName, lastName)
+}
+
 
 func TestQueryParameter(t *testing.T){
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?name=Ichigo", nil)
 	recorder := httptest.NewRecorder()
 
 	SayHello(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func TestMultipleQueryParameter(t *testing.T){
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080?first_name=Ichgio&last_name=Kurosaki", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleQueryParameter(recorder, request)
 
 	response := recorder.Result()
 	body, _ := io.ReadAll(response.Body)
