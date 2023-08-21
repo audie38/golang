14d2527a57014f0875d1f8d34286156cb0106563
Audie Milson <milson.audie@gmail.com>
2023-08-21 22:15:54 +0700

    http.NewServeMux (combine multiple HandleFunc)  example

--- golang-web/server_test.go
+++ golang-web/server_test.go
@@ -7,13 +7,20 @@ import (
 )
 
 func TestServer(t *testing.T) {
-	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request){
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "OK!")
+	})
+	mux.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request){
 		fmt.Fprint(writer, "Hello World")
-	}
+	})
+	mux.HandleFunc("/hello/go", func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "Golang Web Hello World")
+	})
 
 	server := http.Server{
 		Addr: "localhost:8080",
-		Handler: handler,
+		Handler: mux,
 	}
 
 	err := server.ListenAndServe()
