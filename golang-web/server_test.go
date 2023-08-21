4a3aafb8e4938d894f75879e2c038dcfd1d369e1
Audie Milson <milson.audie@gmail.com>
2023-08-21 22:04:26 +0700

    http HandlerFunc example

--- golang-web/server_test.go
+++ golang-web/server_test.go
@@ -1,13 +1,19 @@
 package golang_web
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
 
 func TestServer(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request){
+		fmt.Fprint(writer, "Hello World")
+	}
+
 	server := http.Server{
 		Addr: "localhost:8080",
+		Handler: handler,
 	}
 
 	err := server.ListenAndServe()
