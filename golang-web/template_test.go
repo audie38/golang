5f6aecb5fc64fdb1b23940b759dd150337758c69
Audie Milson <milson.audie@gmail.com>
2023-08-22 13:24:23 +0700

    Golang Web Templating

--- golang-web/template_test.go
+++ golang-web/template_test.go
@@ -20,7 +20,31 @@ func SimpleHTML(w http.ResponseWriter, r *http.Request){
 		panic(err)
 	}
 
-	t.ExecuteTemplate(w, "SIMPLE", `<div class="container my-5">Hello Golang</div>`)	
+	t.ExecuteTemplate(w, "SIMPLE", `Hello Golang`)	
+}
+
+func SimpleHTMLFile(w http.ResponseWriter, r *http.Request){
+	t, err := template.ParseFiles("./templates/simple.gohtml")
+	if err != nil{
+		panic(err)
+	}
+	t.ExecuteTemplate(w, "simple.gohtml", `<div className="container my-5">Hello Golang Web Templates</div>`)
+}
+
+func TestTemplateLayout(t *testing.T){
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", SimpleHTML)
+	mux.HandleFunc("/template", SimpleHTMLFile)
+
+	server := http.Server{
+		Addr: "localhost:8000",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil{
+		t.Fatal(err)
+	}
 }
 
 func TestTemplate(t *testing.T){
@@ -28,6 +52,16 @@ func TestTemplate(t *testing.T){
 	recorder := httptest.NewRecorder()
 
 	SimpleHTML(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateHTML(t *testing.T){
+	request := httptest.NewRequest(http.MethodGet, BASE_URL, nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHTMLFile(recorder, request)
 
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
