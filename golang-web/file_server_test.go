8b14a9f01623d966ba937f4a598dd58c77f1e597
Audie Milson <milson.audie@gmail.com>
2023-08-22 12:37:49 +0700

    Implement golang embed to file server

--- golang-web/file_server_test.go
+++ golang-web/file_server_test.go
@@ -1,6 +1,8 @@
 package golang_web
 
 import (
+	"embed"
+	"io/fs"
 	"net/http"
 	"testing"
 )
@@ -12,6 +14,27 @@ func TestFileServer(t *testing.T){
 	mux := http.NewServeMux()
 	mux.Handle("/", fileServer)
 	// mux.Handle("/static", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr: "localhost:8000",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil{
+		t.Fatal(err)
+	}
+}
+
+//go:embed public
+var public embed.FS
+
+func TestFileServerGolangEmbed(t *testing.T){
+	directory, _ := fs.Sub(public, "public")
+	fileServer := http.FileServer(http.FS(directory))
+	// fileServer := http.FileServer(http.FS(public))
+	mux := http.NewServeMux()
+	mux.Handle("/public/", http.StripPrefix("/public", fileServer))
 
 	server := http.Server{
 		Addr: "localhost:8000",
