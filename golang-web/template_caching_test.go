4625291534e2de88b0cb99ca047c76c27e7854ed
Audie Milson <milson.audie@gmail.com>
2023-08-24 20:41:50 +0700

    Template Auto Escape function

--- golang-web/template_caching_test.go
+++ golang-web/template_caching_test.go
@@ -19,6 +19,20 @@ func TemplateCaching(w http.ResponseWriter, r *http.Request){
 	myTemplates.ExecuteTemplate(w, "simple.gohtml", "Template Caching")
 }
 
+type PageContentData struct{
+	Title string
+	Body interface{}
+}
+
+func TemplateAutoEscape(w http.ResponseWriter, r *http.Request){
+	data := PageContentData{
+		Title: "Go-Lang Auto Escape",
+		Body: `<div classname="container my-5 text-center"><p>Go-Lang Auto Escape</p></div>`,
+	}
+
+	myTemplates.ExecuteTemplate(w, "post", data)
+}
+
 func TestTemplateCaching(t *testing.T){
 	request := httptest.NewRequest(http.MethodGet, BASE_URL, nil)
 	recorder := httptest.NewRecorder()
@@ -29,9 +43,20 @@ func TestTemplateCaching(t *testing.T){
 	fmt.Println(string(body))
 }
 
+func TestTemplateAutoEscape(t *testing.T){
+	request := httptest.NewRequest(http.MethodGet, BASE_URL, nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateAutoEscape(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
 func TestRunTemplateCaching(t *testing.T){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", TemplateCaching)
+	mux.HandleFunc("/escape", TemplateAutoEscape)
 
 	server := http.Server{
 		Addr: "localhost:8000",
