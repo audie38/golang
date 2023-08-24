859e0c264accb8ad1f594f0778f2c87aeba73384
Audie Milson <milson.audie@gmail.com>
2023-08-24 19:22:23 +0700

    Template Function example

---
+++ golang-web/template_function_test.go
@@ -0,0 +1,51 @@
+package golang_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type MyPage struct {
+	Name string
+}
+
+func (myPage MyPage) SayHello(name string) string {
+	return "Hello " + name + ", My Name is " + myPage.Name
+}
+
+func TemplateFunction(w http.ResponseWriter, r *http.Request){
+	t := template.Must(template.New("FUNCTION").Parse(`{{ .SayHello "Ichigo" }}`))
+	t.ExecuteTemplate(w, "FUNCTION", MyPage{
+		Name: "Kurosaki",
+	})
+
+}
+
+func TestTemplateFunction(t *testing.T){
+	request := httptest.NewRequest(http.MethodGet, BASE_URL, nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunction(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestRunTemplate(t *testing.T){
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", TemplateFunction)
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: mux,
+	}	
+
+	err := server.ListenAndServe()
+	if err != nil{
+		t.Fatal(err)
+	}
+}
