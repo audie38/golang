229a0420e76724667a94d02eafd8561020ccb55b
Audie Milson <milson.audie@gmail.com>
2023-08-26 19:35:04 +0700

    Encode Struct to JSON example

---
+++ golang-json/json_test.go
@@ -0,0 +1,38 @@
+package golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type Customer struct{
+	FirstName string
+	MiddleName string
+	LastName string
+	Age int
+	IsHeadCaptain bool
+}
+
+func logJson(data interface{}) {
+	bytes, err := json.Marshal(data)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(string(bytes))
+}
+
+func TestEncode(t *testing.T){
+	logJson("Genryusai")
+	logJson(1)
+	logJson(true)
+	logJson([]string{"Shigekuni", "Genryusai", "Yamamoto"})
+	customer := Customer{
+		FirstName: "Yamamoto",
+		MiddleName: "Shigekuni",
+		LastName: "Genryusai",
+		Age: 1000,
+		IsHeadCaptain: true,
+	}
+	logJson(customer)
+}
