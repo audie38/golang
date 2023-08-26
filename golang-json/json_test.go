58cb7db69a7ba015284269e541b74756b1a48246
Audie Milson <milson.audie@gmail.com>
2023-08-26 20:06:39 +0700

    JSON Tag Encode Decode example

--- golang-json/json_test.go
+++ golang-json/json_test.go
@@ -20,6 +20,12 @@ type Customer struct{
 	Addreses []Address
 }
 
+type Product struct{
+	Id string	`json:"id"`
+	Name string	`json:"name"`
+	ImageURL string	`json:"image_url"`
+}
+
 func logJson(data interface{}) {
 	bytes, err := json.Marshal(data)
 	if err != nil{
@@ -65,4 +71,25 @@ func TestDecode(t *testing.T){
 		t.Fatal(err)
 	}
 	fmt.Println(customer)
+}
+
+func TestEncodeTag(t *testing.T){
+	product := Product{
+		Id: "P001",
+		Name: "Product One",
+		ImageURL: "Product One Image",
+	}
+
+	logJson(product)
+}
+
+func TestDecodeTag(t *testing.T){
+	jsonReq := `{"id":"P001","name":"Product One","image_url":"Product One Image"}`
+	jsonBytes := []byte(jsonReq)
+	product := &Product{}
+	err := json.Unmarshal(jsonBytes, product)
+	if err != nil{
+		t.Fatal(err)
+	}
+	fmt.Println(product)
 }
